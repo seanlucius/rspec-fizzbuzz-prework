ac227113c2e00567477e4b74ae607b00b7a5e713
Sean Lucius <seanlucius22@icloud.com>
2019-01-13 03:11:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
+  elsif int % 3 == 0 && int % 5 != 0 
     "Fizz"
-    int % 3 == 0 && int % 5 =! 0 
-    "Fizz"
-    elsif int % 5 == 0 && int % 3
+  elsif int % 5 == 0 && int % 3 != 0
+    "Buzz"
+  else 
+    nil 
   end 
 end 
